339612b86d3926f05f4ddcd3a4fdc23422ba602b
rgio <rob@sapien.network>
2023-12-24 11:50:44 -0700

    fix

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -18,7 +18,6 @@ const handler = async (req: Request): Promise<Response> => {
     //const body = JSON.parse("{\"model\":{\"id\":\"gpt-3.5-turbo\",\"name\":\"GPT-3.5\",\"maxLength\":12000,\"tokenLimit\":4000},\"messages\":[{\"role\":\"user\",\"content\":\"hey there how are you\"},{\"role\":\"assistant\",\"content\":\"Hello! As an AI language model, I don't have feelings or emotions, but I'm here and ready to assist you. How can I help you\"},{\"role\":\"user\",\"content\":\"thanks im doing good\"},{\"role\":\"assistant\",\"content\":\"That's great to hear! If there's anything specific you'd like to discuss or any questions you have, feel free to let me know and I'll do my best to assist you.\"},{\"role\":\"user\",\"content\":\"howdy there partner\"},{\"role\":\"assistant\",\"content\":\"Howdy! How can I assist you today, partner?\"},{\"role\":\"user\",\"content\":\"yall know much bout wranglin\"},{\"role\":\"assistant\",\"content\":\"Well, as an AI language model, I possess a vast amount of knowledge on various subjects, including wrangling. Wrangling typically refers to the skill of managing or handling livestock, particularly horses or cattle. If you have any specific questions or topics related to wrangling, feel free to ask, and I'll do my best to provide you with the information you\"},{\"role\":\"user\",\"content\":\"thats awful nifty\"},{\"role\":\"assistant\",\"content\":\"Thank you! I'm glad you find it nifty. Wrangling is indeed a valuable skill for those working with livestock. It involves tasks such as herding, roping, branding, and other techniques used to handle and care for animals. If you have any particular inquiries or need information on a specific aspect of wrangling, please let me know. I'm here to help!\"},{\"role\":\"user\",\"content\":\"aww ya\"},{\"role\":\"assistant\",\"content\":\"I'm here to assist you, so please let me know if there's anything specific you'd like to know or discuss about wrangling, or any other topic you're interested in.\"},{\"role\":\"user\",\"content\":\"how does this work\"}],\"key\":\"\",\"prompt\":\"You are ChatGPT, a large language model trained by OpenAI. Follow the user\'s instructions carefully. Respond using markdown.\",\"temperature\":1}") as ChatBody;
     const body = await req.json() as ChatBody;
     const { model, messages, key, prompt, temperature } = body;
-    console.log(`BODY IS: ${JSON.stringify(body)}`);
 
     const path = 'test.pdf';
     const embeddingEndpoint = 'http://localhost:5000/chat_document'; // Replace [FLASK_SERVER_URL] with your Flask server's URL
@@ -33,12 +32,10 @@ const handler = async (req: Request): Promise<Response> => {
       })
     });
     const json = await embeddingResponse.json();
-    console.log(`DATA IS: ${JSON.stringify(json)}`);
+    //.log(`DATA IS: ${JSON.stringify(json)}`);
     return new Response(JSON.stringify({data : json.response }), { status: 200 });
     // const { model, messages, prompt, temperature } = (await req.json()) as ChatBody;
     //const key = process.env.OPENAI_API_KEY
-    // const key = "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";
-    // console.log(`KEY IS: ${key}`);
 
     // await init((imports) => WebAssembly.instantiate(wasm, imports));
     // const encoding = new Tiktoken(
